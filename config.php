aa3ad28122cde8a7908cb7ad397241acbc74e445
phukongka <aphukong@gmail.com>
2014-05-09 15:55:43 +0700

    config apply

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$link =mysql_pconnect("root","chai","123456");
+$link =mysql_pconnect("root","chai","123456");
+
+class Myclass extends Base{
+	
+	public function getFunction1(){
+		for($i=0;$i<100;$i++){
+			
+		}
+		
+	}
+	
+	
+}
